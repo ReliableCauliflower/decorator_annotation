034b476d3ee8f2b135f3e0eb6e0dfbe00f467561
Eugene Ostashko <ostashko4@gmail.com>
2021-01-22 18:41:44 +0200

    Add ignore where any and method predicates

--- lib/decorator_annotation.dart
+++ lib/decorator_annotation.dart
@@ -5,9 +5,39 @@ import 'dart:async';
 const String parametersFieldName = 'parameters';
 const String wrapperFieldName = 'wrapper';
 const String forAllWhereAnyFieldName = 'forAllWhereAny';
+const String ignoreWhereAnyFieldName = 'ignoreWhereAny';
 const String ignoreFieldName = 'ignore';
 
-enum MethodPredicate { hasOverride }
+enum MethodPredicate {
+  hasImplicitReturnType,
+  isAbstract,
+  isSynchronous,
+  isAsynchronous,
+  isExternal,
+  isGenerator,
+  isOperator,
+  isStatic,
+  hasAlwaysThrows,
+  hasDeprecated,
+  hasDoNotStore,
+  hasFactory,
+  hasInternal,
+  hasIsTest,
+  hasIsTestGroup,
+  hasJS,
+  hasLiteral,
+  hasMustCallSuper,
+  hasNonVirtual,
+  hasOptionalTypeArgs,
+  hasOverride,
+  hasProtected,
+  hasRequired,
+  hasSealed,
+  hasVisibleForTemplate,
+  hasVisibleForTesting,
+  isPrivate,
+  isPublic,
+}
 
 typedef WrapperFunction<T> = FutureOr<T> Function(
   FutureOr<T> Function() body, {
@@ -18,11 +48,13 @@ class DecoratorClass<T> {
   final WrapperFunction<T> wrapper;
   final dynamic parameters;
   final Set<Set<MethodPredicate>> forAllWhereAny;
+  final Set<Set<MethodPredicate>> ignoreWhereAny;
 
   const DecoratorClass({
     this.wrapper,
     this.parameters,
     this.forAllWhereAny,
+    this.ignoreWhereAny,
   }) : assert(
           (wrapper == null && forAllWhereAny == null) || wrapper != null,
           'forAllWhereAny can only be used with provided wrapper',
