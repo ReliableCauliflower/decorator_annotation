17bcc0de1b0e313db3f20cd5a7add6890487bc3e
Eugene Ostashko <ostashko4@gmail.com>
2021-01-22 15:55:22 +0200

    Add Decorator ignore and DecoratorClass forAllWhereAny

--- lib/decorator_annotation.dart
+++ lib/decorator_annotation.dart
@@ -4,6 +4,10 @@ import 'dart:async';
 
 const String parametersFieldName = 'parameters';
 const String wrapperFieldName = 'wrapper';
+const String forAllWhereAnyFieldName = 'forAllWhereAny';
+const String ignoreFieldName = 'ignore';
+
+enum MethodPredicate { hasOverride }
 
 typedef WrapperFunction<T> = FutureOr<T> Function(
   FutureOr<T> Function() body, {
@@ -13,13 +17,26 @@ typedef WrapperFunction<T> = FutureOr<T> Function(
 class DecoratorClass<T> {
   final WrapperFunction<T> wrapper;
   final dynamic parameters;
+  final Set<Set<MethodPredicate>> forAllWhereAny;
 
-  const DecoratorClass({this.wrapper, this.parameters});
+  const DecoratorClass({
+    this.wrapper,
+    this.parameters,
+    this.forAllWhereAny,
+  }) : assert(
+          (wrapper == null && forAllWhereAny == null) || wrapper != null,
+          'forAllWhereAny can only be used with provided wrapper',
+        );
 }
 
 class Decorator<T> {
   final WrapperFunction<T> wrapper;
   final dynamic parameters;
+  final bool ignore;
 
-  const Decorator({this.wrapper, this.parameters});
+  const Decorator({
+    this.wrapper,
+    this.parameters,
+    this.ignore = false,
+  });
 }
